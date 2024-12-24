8d011df081a2a715985f5b6d29ed3dcad3b2fc2a
DELL <DELL@DESKTOP-1QE061J>
2024-12-24 20:10:16 +0530

    java8 stream

--- src/java8StreamProgram/ArmStrongNumber.java
+++ src/java8StreamProgram/ArmStrongNumber.java
@@ -1,9 +1,14 @@
 package java8StreamProgram;
 
+import java.util.Scanner;
+
 public class ArmStrongNumber {
 
 	public static void main(String[] args) {
-		int num = 153;
+		//int num = 153;
+		Scanner n = new Scanner(System.in);
+		System.out.println("Enter your number:");
+		int num = n.nextInt();
 		int digits = String.valueOf(num).length();
 		int sum = String.valueOf(num).chars()
 				.map(ch ->(int)Math.pow(Character.getNumericValue(ch), digits))
